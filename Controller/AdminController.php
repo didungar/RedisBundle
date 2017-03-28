0fbe54d88f03c75715c7e319d625a7060c8490b6
Didier Ungar <didungar@gmail.com>
2017-03-28 22:05:30 +0200

    reindention propre

--- Controller/AdminController.php
+++ Controller/AdminController.php
@@ -14,77 +14,77 @@ class AdminController extends Controller
 {
 
 	protected function actionForm() {
-        	if ( !empty($_POST) ) {
+			if ( !empty($_POST) ) {
 			$oRedis = new \Redis();
 			$oRedis->connect('localhost');
-        	        switch(@$_POST['act']) {
-        	                case 'get' :
-        	                        var_dump($_POST['key']);
-        	                        var_dump($oRedis->get($_POST['key']));
-        	                        break;
-        	                case 'set' :
-        	                        var_dump($_POST['key']);
-        	                        var_dump($_POST['value']);
-                                    if ( $_POST['ttl']>0 ) {
-                                       var_dump($oRedis->set($_POST['key'], $_POST['value'], (int)$_POST['ttl']));
-                                    } else {
-        	                           var_dump($oRedis->set($_POST['key'], $_POST['value']));
-                                    }
-        	                        break;
+					switch(@$_POST['act']) {
+							case 'get' :
+									var_dump($_POST['key']);
+									var_dump($oRedis->get($_POST['key']));
+									break;
+							case 'set' :
+									var_dump($_POST['key']);
+									var_dump($_POST['value']);
+									if ( $_POST['ttl']>0 ) {
+									   var_dump($oRedis->set($_POST['key'], $_POST['value'], (int)$_POST['ttl']));
+									} else {
+									   var_dump($oRedis->set($_POST['key'], $_POST['value']));
+									}
+									break;
 				case 'delete' :
 					var_dump($_POST['key']);
 					var_dump($oRedis->delete($_POST['key']));
 					break;
-        	        }
-        	}
+					}
+			}
 	}
 
-    /**
-     * @Route("/")
-     * @Template()
-     */
-    public function indexAction()
-    {
-	$oRedis = new \Redis();
-	$oRedis->connect('localhost');
-        $this->actionForm();
-	return [
-		'keys_count' => $oRedis->dbSize(),
-	];
-    }
-    /**
-     * @Route("/keys")
-     * @Template()
-     */
-    public function keysAction()
-    {
-        $oRedis = new \Redis();
-        $oRedis->connect('localhost');
-	$this->actionForm();
-        $keys = $oRedis->keys('*');
-	$lstKeys = [];
-	foreach($keys as $key) {
-		$lstKeys[$key] = [
-			'key' => $key,
-			'value' => $oRedis->get($key),
-			'ttl' => $oRedis->ttl($key),
+	/**
+	 * @Route("/")
+	 * @Template()
+	 */
+	public function indexAction()
+	{
+		$oRedis = new \Redis();
+		$oRedis->connect('localhost');
+		$this->actionForm();
+		return [
+			'keys_count' => $oRedis->dbSize(),
 		];
 	}
+	/**
+	 * @Route("/keys")
+	 * @Template()
+	 */
+	public function keysAction()
+	{
+		$oRedis = new \Redis();
+		$oRedis->connect('localhost');
+	   $this->actionForm();
+		$keys = $oRedis->keys('*');
+		$lstKeys = [];
+		foreach($keys as $key) {
+			$lstKeys[$key] = [
+				'key' => $key,
+				'value' => $oRedis->get($key),
+				'ttl' => $oRedis->ttl($key),
+			];
+		}
 var_dump($lstKeys);
-        return [
+		return [
 		'lstKeys' => $lstKeys,
 	];
-    }
-    /**
-     * @Route("/info")
-     * @Template()
-     */
-    public function infoAction()
-    {
-        $oRedis = new \Redis();
-        $oRedis->connect('localhost');
-        var_dump($oRedis->info());
-        exit;
-        return [];
-    }
+	}
+	/**
+	 * @Route("/info")
+	 * @Template()
+	 */
+	public function infoAction()
+	{
+		$oRedis = new \Redis();
+		$oRedis->connect('localhost');
+		var_dump($oRedis->info());
+		exit;
+		return [];
+	}
 }
